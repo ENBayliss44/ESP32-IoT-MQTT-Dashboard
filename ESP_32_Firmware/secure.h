d7c7f414289e03b22879ce5eca6be02c62c8f48e
Edward Bayliss <118219451+ENBayliss44@users.noreply.github.com>
2025-08-31 09:32:58 +0100

    Delete ESP_32_Firmware/secure.h

--- ESP_32_Firmware/secure.h
+++
@@ -1,9 +0,0 @@
-#ifndef SECURE_H
-#define SECURE_H
-
-// Replace with your Wi-Fi and MQTT details
-const char* WIFI_SSID = "your_wifi_ssid";
-const char* WIFI_PASSWORD = "your_wifi_password";
-const char* MQTT_SERVER = "192.168.1.xxx"; // Your broker's IP
-
-#endif
